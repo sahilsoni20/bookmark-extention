5b317d58530d14c8a566a855aa126c2353c7dd24
sahilsoni20 <ssahil3120@gmail.com>
2024-06-02 10:37:28 +0530

    Update bookmark.jsx

--- src/components/bookmark/bookmark.jsx
+++ src/components/bookmark/bookmark.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { FaTrash } from 'react-icons/fa';
+import { FiPlusCircle } from "react-icons/fi";
 
 const Bookmark = () => {
   const [bmTitle, setBmTitle] = useState("");
@@ -28,43 +29,45 @@ const Bookmark = () => {
   }
 
   return (
-    <div className=" flex items-center h-screen justify-center ">
-      <section className="bg-[#53b4e4">
+    <div className=" flex items-center h-screen justify-center">
+      <section className="bg-[#53b4e4]  p-4 rounded-xl max-w-54 z-20 relative left-1/3 top-24 ">
         <div>
-          <form onSubmit={addBookmark}>
-            <div className="flex">
+          <form onSubmit={addBookmark} className="text-[#d9ecf6]">
+            <div className="mb-3">
               <label htmlFor="bmTitle">Title</label>
               <input
                 type="text"
                 id="bmTitle"
+                className="ml-2 p-1 border-b focus:bg-transparent focus:border-none text-white bg-transparent"
                 value={bmTitle}
                 onChange={(e) => setBmTitle(e.target.value)}
               />
             </div>
-            <div>
+            <div className="mb-3">
               <label htmlFor="bmLink">URL</label>
               <input
                 type="text"
                 id="bmLink"
+                className="ml-3.5 p-1 border-b focus:bg-transparent focus:border-none text-white bg-transparent"
                 value={bmLink}
                 onChange={(e) => setBmLink(e.target.value)}
-                className="border-black border-b-5"
               />
             </div>
-            <button type="submit">Add Bookmark</button>
+            <button type="submit" className="flex items-center ml-20 bg-[#b0e5ff] text-[#48baf4] p-2 rounded-full font-medium">Add<FiPlusCircle size={18} className="ml-2"/></button>
           </form>
         </div>
       </section>
 
-      <section className="bg-[#b5e6ff] max-w-lg rounded-xl p-8 max-h-lg w-full h-3/5">
-        <header>
+      <section className="bg-[#b5e6ff] rounded-xl p-8 w-full max-w-md h-auto max-h-full">
+        <header className="text-[#48baf4]">
           <h3 className="flex justify-center text-3xl">BookMarks</h3>
+          <FiPlusCircle color="green" size={25} className="float-right bottom-7 cursor-pointer relative"/>
         </header>
         <div className="mt-6">
-          <ul className="grid grid-cols-5 gap-6">
+          <ul className="grid grid-cols-4 gap-x-20 gap-y-8 mr-20 relative">
             {BookMarks.map((bm) => {
               return (
-                <li key={bm.id} className="flex">
+                <li key={bm.id} className="flex bg-[#84d6ff] p-2 w-20 h-18 rounded-lg">
                   <a href={bm.bmLink} rel="">
                     <img
                       src={`https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${bm.bmLink}&size=40`}
@@ -72,7 +75,7 @@ const Bookmark = () => {
                     />
                     <span>{bm.bmTitle}</span>
                   </a>
-                  <FaTrash onClick={() => deleteBookMark(bm.id)} size={15} className="relative left-2"/>
+                  <FaTrash onClick={() => deleteBookMark(bm.id)} size={12} className="mr-2 absolute ml-12 text-red-500"/>
                 </li>
               );
             })}
